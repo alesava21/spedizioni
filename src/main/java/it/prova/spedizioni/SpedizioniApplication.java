ddb5a89923bb1aeb7df13b7f01e3ec5a73810abb
alesava21 <alexandru.sava521@gmail.com>
2023-01-25 10:26:52 +0100

    primo commit

---
+++ src/main/java/it/prova/spedizioni/SpedizioniApplication.java
@@ -0,0 +1,13 @@
+package it.prova.spedizioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpedizioniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpedizioniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/prova/spedizioni/SpedizioniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.spedizioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpedizioniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
